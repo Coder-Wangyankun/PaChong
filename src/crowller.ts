01e8b9d3b7487cb594e3f7133e029e7a5af3fb1d
yankun.wang <coder_wangyankun@163.com>
2023-02-22 17:22:01 +0800

    使用组合模式抽象代码

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,37 +1,14 @@
-import superagent from "superagent";
-import cheerio from "cheerio";
 import fs from "fs";
 import path from "path";
+import DellAnalyzer from "./dellAnalyzer";
+import superagent from "superagent";
 
-interface Course {
-  title: string;
-  count: number;
-}
-
-interface CourseResult {
-  time: number;
-  data: Course[];
-}
-
-// json 文件的数据结构
-interface JsonContent {
-  [propName: number]: Course[];
+export interface Analyzer {
+  analyze: (html: string, filePath: string) => string;
 }
 
 class Crowller {
-  // private secret = "secretKey";
-  // private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
-  private url = `http://www.dell-lee.com/typescript/demo.html?secret=secretKey`;
-  // private url = "https://ssr1.scrape.center/"; // 猫眼电影数据网
   private filePath = path.resolve(__dirname, "../data/course.json");
-
-  // 初始化爬虫
-  async initSpiderProcess() {
-    const html = await this.getRawHtml();
-    const courseInfo = this.getCourseInfo(html);
-    const fileContent = this.generateJsonContent(courseInfo);
-    this.writeFile(JSON.stringify(fileContent));
-  }
 
   // 爬取html
   async getRawHtml() {
@@ -39,36 +16,11 @@ class Crowller {
     return result.text; // 返回 html 文本
   }
 
-  // 处理 html 格式数据
-  getCourseInfo(html: string) {
-    const $ = cheerio.load(html);
-    const courseItems = $(".course-item"); // 页面上每一个课程区块都是一个 class 是 .course-item 的 div
-    const courseInfos: Course[] = [];
-    courseItems.map((item, element) => {
-      const descs = $(element).find(".course-desc"); // jquery find() 方法用于查找所有后代元素
-      const title = descs.eq(0).text(); // eq() 方法返回匹配元素集上相应索引的元素；text() 方法返回所选元素的文本内容
-      const count = parseInt(descs.eq(1).text().split("：")[1], 10);
-      courseInfos.push({
-        title,
-        count,
-      });
-    });
-    const result = {
-      time: new Date().getTime(),
-      data: courseInfos,
-    };
-    return result;
-  }
-
-  // 数据进一步处理成写入 json 文件中格式
-  generateJsonContent(courseInfo: CourseResult) {
-    const filePath = this.filePath;
-    let fileContent: JsonContent = {};
-    if (fs.existsSync(filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    }
-    fileContent[courseInfo.time] = courseInfo.data;
-    return fileContent;
+  // 初始化爬虫
+  async initSpiderProcess() {
+    const html = await this.getRawHtml();
+    const fileContent = analyzer.analyze(html, this.filePath); // html 传给分析器分析，返回 string 类型 json
+    this.writeFile(fileContent);
   }
 
   // 将数据写入到文件中
@@ -76,8 +28,10 @@ class Crowller {
     fs.writeFileSync(this.filePath, content);
   }
 
-  constructor() {
+  constructor(private url: string, private analyzer: any) {
     this.initSpiderProcess();
   }
 }
-new Crowller();
+const url = "http://www.dell-lee.com/typescript/demo.html?secret=secretKey";
+const analyzer = new DellAnalyzer(); // 生成分析器
+new Crowller(url, analyzer); // 传入 url 和分析器
---
+++ src/dellAnalyzer.ts
@@ -0,0 +1,59 @@
+// 此文件专门处理逻辑
+import cheerio from "cheerio";
+import fs from "fs";
+import { Analyzer } from "./crowller";
+
+interface Course {
+  title: string;
+  count: number;
+}
+
+interface CourseResult {
+  time: number;
+  data: Course[];
+}
+
+// json 文件的数据结构
+interface JsonContent {
+  [propName: number]: Course[];
+}
+
+export default class DellAnalyzer implements Analyzer {
+  // 处理 html 格式数据
+  private getCourseInfo(html: string) {
+    const $ = cheerio.load(html);
+    const courseItems = $(".course-item"); // 页面上每一个课程区块都是一个 class 是 .course-item 的 div
+    const courseInfos: Course[] = [];
+    courseItems.map((item, element) => {
+      const descs = $(element).find(".course-desc"); // jquery find() 方法用于查找所有后代元素
+      const title = descs.eq(0).text(); // eq() 方法返回匹配元素集上相应索引的元素；text() 方法返回所选元素的文本内容
+      const count = parseInt(descs.eq(1).text().split("：")[1], 10);
+      courseInfos.push({
+        title,
+        count,
+      });
+    });
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos,
+    };
+    return result;
+  }
+
+  // 数据进一步处理成写入 json 文件中格式
+  private generateJsonContent(courseInfo: CourseResult, filePath: string) {
+    let fileContent: JsonContent = {};
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    }
+    fileContent[courseInfo.time] = courseInfo.data;
+    return JSON.stringify(fileContent);
+  }
+  // 分析器入口
+  public analyze(html: string, filePath: string): string {
+    const courseInfo = this.getCourseInfo(html);
+    const result = this.generateJsonContent(courseInfo, filePath);
+    return result;
+  }
+  constructor() {}
+}
